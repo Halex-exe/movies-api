14b1628d14ddf38acd30f51f1ece0e0e68ced787
Alexandre Martins Klafke <alexandre.klafke@terceiroslinx.com.br>
2022-04-07 00:17:44 -0300

    First Commit.

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            movie.Id = id++;
+            movies.Add(movie);
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> GetMovie()
+        {
+            return movies;
+        }
+
+        [HttpGet("{id}")]
+        public Movie GetMovieById(int id)
+        {
+            foreach (var movie in movies)
+            {
+                if (movie.Id == id)
+                {
+                    return movie;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Movie title is mandatory")]
+        public string Title { get; set; }
+        [Required]
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        [Range(1, 600)]
+        public int Duration { get; set; }
+    }
+}
